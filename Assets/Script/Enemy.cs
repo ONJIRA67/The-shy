676c77868fc555d489822f0e7f7a18149c5b2b04
kanchanit <kanchanit0@gmail.com>
2023-03-02 15:54:38 +0700

    Add stop follow player
    - Add script enemy stop following player when gets closer
    - Change animation when enemy stop

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -8,18 +8,46 @@ public class Enemy : MonoBehaviour
     public GameObject player;
     public float speed;
 
+    public float stoppingDistance;
+    public float retreatDistance;
+
+    private Animation animator;
+
     void Start()
     {
         enemyRb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animation>();
     }
 
     void Update()
     {
         // Enemy is moving to player with look direction
-        Vector3 lookDirection = (player.transform.position - transform.position).normalized;
+        Vector3 lookDirection = player.transform.position - transform.position;
+        float distanceToPlayer = lookDirection.magnitude;
+
         Quaternion lookRotation = Quaternion.LookRotation(new Vector3(lookDirection.x, 0f, lookDirection.z));
         transform.rotation = lookRotation;
 
-        enemyRb.AddForce(lookDirection * speed);
+        if (distanceToPlayer > stoppingDistance)
+        {
+            enemyRb.velocity = lookDirection.normalized * speed;
+        }
+        else if (distanceToPlayer < retreatDistance)
+        {
+            enemyRb.velocity = -lookDirection.normalized * speed;
+        }
+        else
+        {
+            enemyRb.velocity = Vector3.zero;
+            animator.Play("Attack1");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            enemyRb.velocity = Vector3.zero;
+        }
     }
 }
