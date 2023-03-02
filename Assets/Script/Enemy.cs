ba569a28b2907b67ae4541e73c2af632c5db025d
kanchanit <kanchanit0@gmail.com>
2023-03-02 12:09:11 +0700

    Add ghost sound effect
    - Add asset ghost audio
    - Add script sound louder when the enemy gets closer

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -7,23 +7,30 @@ public class Enemy : MonoBehaviour
     private Rigidbody enemyRb;
     public GameObject player;
     public float speed;
+    public float maxDistance = 10.0f;
+
+    private AudioSource audioSource;
 
     void Start()
     {
         enemyRb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     void Update()
     {
+        // Enemy is moving to player with look direction
         Vector3 lookDirection = (player.transform.position - transform.position).normalized;
         Quaternion lookRotation = Quaternion.LookRotation(new Vector3(lookDirection.x, 0f, lookDirection.z));
         transform.rotation = lookRotation;
+
         enemyRb.AddForce(lookDirection * speed);
 
+        // The sound is playing louder when gets closer to player
+        float distance = Vector3.Distance(player.transform.position, transform.position);
+        float volume = Mathf.InverseLerp(maxDistance, 0.0f, distance);
+        volume = Mathf.Clamp01(volume);
 
-        // if (transform.position.y < -10)
-        // {
-        //     Destroy(gameObject);
-        // }
+        audioSource.volume = volume;
     }
 }
