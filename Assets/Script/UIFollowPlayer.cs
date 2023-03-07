621094c7db3f4ff6bac57941044f76412d2a5ad8
kanchanit <kanchanit0@gmail.com>
2023-03-07 16:38:38 +0700

    Change position start menu

--- Assets/Script/UIFollowPlayer.cs
+++ Assets/Script/UIFollowPlayer.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class UIFollowPlayer : MonoBehaviour
 {
-    public GameObject startPanel;
     public GameObject healthPanel;
     public GameObject finishPanel;
     [SerializeField] private Transform player;
