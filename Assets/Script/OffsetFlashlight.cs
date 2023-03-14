7272b781749375f60f9cdde485485de8238ef4bd
ONJIRA67 <74503894+ONJIRA67@users.noreply.github.com>
2023-03-14 14:11:34 +0700

    Change light position

--- Assets/Script/OffsetFlashlight.cs
+++ Assets/Script/OffsetFlashlight.cs
@@ -8,17 +8,23 @@ public class OffsetFlashlight : MonoBehaviour
     private Vector3 vectOffest;
     private GameObject goFollow;
     [SerializeField] private float speed = 3.0f;
+    [SerializeField] private Transform player;
+    [SerializeField] private Vector3 offset;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        goFollow = Camera.main.gameObject;
-        vectOffest = transform.position = goFollow.transform.position;
+        // goFollow = Camera.main.gameObject;
+        // vectOffest = transform.position = goFollow.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = goFollow.transform.position + vectOffest;
-        transform.rotation = Quaternion.Slerp(transform.rotation, goFollow.transform.rotation, speed * Time.deltaTime);
+        // transform.position = goFollow.transform.position + vectOffest;
+        // transform.rotation = Quaternion.Slerp(transform.rotation, goFollow.transform.rotation, speed * Time.deltaTime);
+        transform.rotation = player.transform.rotation;
+        transform.position = player.transform.position + player.transform.rotation * offset;
     }
 }
